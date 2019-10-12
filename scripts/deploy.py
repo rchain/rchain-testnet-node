5f79e0b718850ccb92b9f44e0faabbc85c672777
Nutzipper <nomail@nodomain.nozone>
2019-10-12 21:23:20 +0300

    loggin fix + allow NoNewDeploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,11 @@
-import sys
 import grpc
+import logging
+import sys
 from rchain.crypto import PrivateKey
 from rchain.client import RClient
 
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
 with grpc.insecure_channel('localhost:40401') as channel:
 	client = RClient(channel)
 	with open(sys.argv[2]) as file:
--- scripts/propose.py
+++ scripts/propose.py
@@ -1,6 +1,17 @@
 import grpc
+import logging
 from rchain.client import RClient
+from rchain.client import RClientException
 
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+error=0
 with grpc.insecure_channel('localhost:40401') as channel:
 	client = RClient(channel)
-	client.propose()
+	try:
+		client.propose()
+	except RClientException as e:
+		# NoNewDeploys is a valid scenario
+		if "NoNewDeploys" not in str(e):
+			error=1
+print(error)
