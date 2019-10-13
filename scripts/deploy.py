3896f7cfeea3ca677af9e5e64d172e3c4bc7b13b
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-10-13 13:36:45 +0200

    Replace tabs with spaces in Python scripts (PEP 8)

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,7 +7,7 @@ from rchain.client import RClient
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 with grpc.insecure_channel('localhost:40401') as channel:
-	client = RClient(channel)
-	with open(sys.argv[2]) as file:
-		data = file.read()
-		client.deploy(PrivateKey.from_hex(sys.argv[1]), data, 1, 1000000000)
+    client = RClient(channel)
+    with open(sys.argv[2]) as file:
+        data = file.read()
+        client.deploy(PrivateKey.from_hex(sys.argv[1]), data, 1, 1000000000)
--- scripts/propose.py
+++ scripts/propose.py
@@ -8,11 +8,11 @@ root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 error=0
 with grpc.insecure_channel('localhost:40401') as channel:
-	client = RClient(channel)
-	try:
-		client.propose()
-	except RClientException as e:
-		# NoNewDeploys is a valid scenario
-		if "NoNewDeploys" not in str(e):
-			error=1
+    client = RClient(channel)
+    try:
+        client.propose()
+    except RClientException as e:
+        # NoNewDeploys is a valid scenario
+        if "NoNewDeploys" not in str(e):
+            error=1
 sys.exit(error)
