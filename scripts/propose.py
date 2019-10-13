c02346f51f6252b9ed76e0338f11319af38a5c9e
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-10-13 13:51:12 +0200

    propose.py: Raise unexpected exceptions

--- scripts/propose.py
+++ scripts/propose.py
@@ -1,12 +1,11 @@
 import grpc
 import logging
-import sys
 from rchain.client import RClient
 from rchain.client import RClientException
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
-error=0
+
 with grpc.insecure_channel('localhost:40401') as channel:
     client = RClient(channel)
     try:
@@ -14,5 +13,4 @@ with grpc.insecure_channel('localhost:40401') as channel:
     except RClientException as e:
         # NoNewDeploys is a valid scenario
         if "NoNewDeploys" not in str(e):
-            error=1
-sys.exit(error)
+            raise
