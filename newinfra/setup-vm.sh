881cf8abfdffef36fe5d3e93ef1a99b10e0fe8b5
GSJ <17771847+gsj5@users.noreply.github.com>
2020-06-08 13:28:29 -0400

    fix wierd directory link issue

--- newinfra/setup-vm.sh
+++ newinfra/setup-vm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/xvdc
-mkdir -p /rchain/rnode 
+mkdir -p /rchain
 mount -o discard,defaults /dev/xvdc /rchain
 echo "UUID=`blkid|grep \"^/dev/xvdc\"|cut -d'\"' -f2` /rchain ext4 discard,defaults,nofail 0 2" >>/etc/fstab
+mkdir -p /rchain/rnode 
 ln -s /rchain/rnode /var/lib/rnode
 mkdir -p /rchain/rnode-static 
 ln -s /rchain/rnode-static /var/lib/rnode-static
