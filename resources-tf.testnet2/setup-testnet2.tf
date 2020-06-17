529b6721e469da7df50427c2fb792659683d423c
GSJ <17771847+gsj5@users.noreply.github.com>
2020-06-17 12:22:34 -0400

    Make testnet nodes larger to accomodate older release and use London datacenter.

--- resources-tf.testnet2/setup-testnet2.tf
+++ resources-tf.testnet2/setup-testnet2.tf
@@ -13,34 +13,34 @@ provider "ibm" {
   
 # Admin keys for root access
 data "ibm_compute_ssh_key" "sre"    	   { label = "rchain-sre-ibm" }
-  
+ 
 data "ibm_security_group" "allow_in_rnode2"{ name = "allow_in_rnode2"}
 data "ibm_security_group" "allow_ssh"      { name = "allow_ssh" }
 data "ibm_security_group" "allow_outbound" { name = "allow_outbound" }
 
-# Finally create some servers
-locals { testnet_list =[{hostname = "node0", datacenter = "lon04"},
-                        {hostname = "node1", datacenter = "fra04"},
-                        {hostname = "node2", datacenter = "wdc04"},
-                        {hostname = "node3", datacenter = "dal05"},
-                        {hostname = "node4", datacenter = "tok04"},
-                        {hostname = "observer", datacenter = "wdc04"} ] }
+locals { 
+   node_count   = "5"   
+   region       = "eu-gb" 
+   datacenter   = "lon04" 
+   domain_name  = "testnet.dev.rchain.coop"
+}
 
+# Finally create some servers
 resource "ibm_compute_vm_instance" "testnet" {
-  count                 = length(local.testnet_list)
-  hostname              = local.testnet_list[count.index].hostname
-  domain                = "testnet.rchain.coop"
-  flavor_key_name       = "U1_4X8X25"
-  datacenter            = local.testnet_list[count.index].datacenter
-  os_reference_code     = "UBUNTU_LATEST"
-  disks                 = ["500"]
-  local_disk                 = false
-  dedicated_acct_host_only   = false
-  ssh_key_ids                = [data.ibm_compute_ssh_key.sre.id ]
+  count                    = local.node_count
+  hostname                 = "node${count.index}"
+  domain                   = local.domain_name
+  flavor_key_name          = "B1_4X16X25"
+  datacenter               = local.datacenter
+  os_reference_code        = "UBUNTU_LATEST"
+  disks                    = ["500"]
+  local_disk               = false
+  dedicated_acct_host_only = false
+  ssh_key_ids                = [data.ibm_compute_ssh_key.sre.id]
   private_security_group_ids = [data.ibm_security_group.allow_ssh.id,
                                 data.ibm_security_group.allow_outbound.id]
   public_security_group_ids  = [data.ibm_security_group.allow_in_rnode2.id,
                                 data.ibm_security_group.allow_ssh.id,
                                 data.ibm_security_group.allow_outbound.id]
   post_install_script_uri    = "https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/newinfra/setup-vm.sh"
-} 
+}
