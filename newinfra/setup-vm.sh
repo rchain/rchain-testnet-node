2f5c81fc9cea747ae11f2add6fbda438238bf194
GSJ <17771847+gsj5@users.noreply.github.com>
2020-06-21 17:25:15 -0400

    not sure why mkdir -p doesn't work during post VM install phase

--- newinfra/setup-vm.sh
+++ newinfra/setup-vm.sh
@@ -2,9 +2,12 @@
 
 # IBM root disk size is max 100G, so link rnode folders to the 2nd larger drive
 mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 /dev/xvdc
-mkdir -p /rchain /rchain/rnode /rchain/rnode-static /rchain/rnode-diag
+mkdir -p /rchain 
 mount -o defaults /dev/xvdc /rchain
 echo "UUID=`blkid|grep \"^/dev/xvdc\"|cut -d'\"' -f2` /rchain ext4 defaults,nofail 0 2" >>/etc/fstab
+mkdir -p /rchain/rnode
+mkdir -p /rchain/rnode-static
+mkdir -p /rchain/rnode-diag
 ln -s /rchain/rnode /var/lib/rnode
 ln -s /rchain/rnode-static /var/lib/rnode-static
 ln -s /rchain/rnode-diag /var/lib/rnode-diag
