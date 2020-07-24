c57dcf18785eed178449cff2af5319fdae07ee3a
GSJ <17771847+gsj5@users.noreply.github.com>
2020-07-24 03:44:13 -0400

    new mainnet validator nodes setup

---
+++ newinfra/setup-mainnet-vm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# IBM root disk size is max 100G, so link rnode folders to the 2nd larger drive
+mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0 /dev/xvdc
+mkdir -p /rchain 
+mount -o defaults /dev/xvdc /rchain
+echo "UUID=`blkid|grep \"^/dev/xvdc\"|cut -d'\"' -f2` /rchain ext4 defaults,nofail 0 2" >>/etc/fstab
+mkdir -p /rchain/rnode
+mkdir -p /rchain/rnode-static
+mkdir -p /rchain/rnode-diag
+ln -s /rchain/rnode /var/lib/rnode
+ln -s /rchain/rnode-static /var/lib/rnode-static
+ln -s /rchain/rnode-diag /var/lib/rnode-diag
+
+# reset FQDN hostname to just its host's name.  Few rnode setup scripts expects this and makes it compatible with GCP
+_hostname="$(hostname)"
+hostnamectl set-hostname "${_hostname%%.*}"
+
+export DEBIAN_FRONTEND=noninteractive
+apt update -q 
+apt -y upgrade
+
+echo "$0:Script done!"
+
