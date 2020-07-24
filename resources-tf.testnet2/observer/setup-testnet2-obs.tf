8e73522f905bc285fcee07f4416e3cbd116ed795
GSJ <17771847+gsj5@users.noreply.github.com>
2020-07-24 03:15:19 -0400

    add 2nd testnet observer

--- resources-tf.testnet2/observer/setup-testnet2-obs.tf
+++ resources-tf.testnet2/observer/setup-testnet2-obs.tf
@@ -35,3 +35,21 @@ resource "ibm_compute_vm_instance" "testnet-obs" {
                                 data.ibm_security_group.allow_outbound.id]
   post_install_script_uri    = "https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/newinfra/setup-vm.sh"
 }
+
+resource "ibm_compute_vm_instance" "testnet-obs2" {
+  hostname                 = "observer-dev"
+  domain                   = "testnet.rchain.coop"
+  flavor_key_name          = "B1_4X16X100"
+  datacenter               = "fra05"
+  os_reference_code        = "UBUNTU_LATEST"
+  disks                    = ["1000"]
+  local_disk               = false
+  dedicated_acct_host_only = false
+  ssh_key_ids                = [data.ibm_compute_ssh_key.sre.id]
+  private_security_group_ids = [data.ibm_security_group.allow_ssh.id,
+                                data.ibm_security_group.allow_outbound.id]
+  public_security_group_ids  = [data.ibm_security_group.allow_in_rnode2.id,
+                                data.ibm_security_group.allow_ssh.id,
+                                data.ibm_security_group.allow_outbound.id]
+  post_install_script_uri    = "https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/newinfra/setup-vm.sh"
+}
