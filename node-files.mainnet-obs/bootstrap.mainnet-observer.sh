a544ec0219c938358ee39c5843c95a79bc03ab29
GSJ <17771847+gsj5@users.noreply.github.com>
2020-07-25 16:22:15 -0400

    Mount IBM Cloud bucket

--- node-files.mainnet-obs/bootstrap.mainnet-observer.sh
+++ node-files.mainnet-obs/bootstrap.mainnet-observer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#
+
+set -e
+
 apt update
 apt -y upgrade 
-apt -y install docker.io docker-compose collectd certbot 
+apt -y install docker.io docker-compose collectd certbot s3fs
+
+# Mount IBM Cloud bucket
+mkdir -p /mnt/rchain-bucket
+s3fs rchain-heapdumps /mnt/rchain-bucket -o url="https://s3.private.eu.cloud-object-storage.appdomain.cloud" -o passwd_file=/root/rchain-bucket.key -o dbglevel=info
 
 curl -o /etc/collectd/collectd.conf  https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/collectd.conf
 curl -o /var/lib/rnode-static/logback.xml https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/logback.xml
