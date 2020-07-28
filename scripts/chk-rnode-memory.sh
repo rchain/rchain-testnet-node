9b332f583f9caf3a1b5cdec4b0e6ff699d2107c5
GSJ <17771847+gsj5@users.noreply.github.com>
2020-07-28 14:37:30 -0400

    cleanup and comment the script

--- scripts/chk-rnode-memory.sh
+++ scripts/chk-rnode-memory.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+#
+#  Script checks if rnode memory exceeds MEM_THRESHOLD %, and if it does, forces jvm to run full garabage collection
+#
+# Redirect all outout to syslog and stderr
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
 MEM_THRESHOLD=85.0
 MEM_USED=$(docker stats --no-stream --format "{{.MemPerc}}" rnode|sed -e 's/%//')
 MEM_LIMIT=$(echo "$MEM_USED > $MEM_THRESHOLD"|bc -l)
+
+echo "rnode used $MEM_USED% memory.  Threshold set at $MEM_THRESHOLD."
 if [[ $MEM_LIMIT -eq 1 ]]
 then
         PID=$(jcmd | grep -v jcmd | awk '{print $1}')
         echo "rnode % memory $MEM_USED > $MEM_THRESHOLD.  Running 'jcmd $PID GC.run'"
         jcmd $PID GC.run
         MEM_USED=$(docker stats --no-stream --format "{{.MemPerc}}" rnode|sed -e 's/%//')
-        echo "rnode % memory now=$MEM_USED"
+        echo "rnode memory now=$MEM_USED%"
 fi
 
