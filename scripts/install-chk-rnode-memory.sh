0e5639a50fe88123d5cc6457c58fc4bc22e728de
GSJ <17771847+gsj5@users.noreply.github.com>
2020-07-28 15:29:21 -0400

    cron.hourly(actually run-parts) doesn't like dot in the filename!

--- scripts/install-chk-rnode-memory.sh
+++ scripts/install-chk-rnode-memory.sh
@@ -2,8 +2,10 @@
 #
 #  Install chk-rnode-memory.sh ias cron to force jvm garbage collection
 #
+set -e
+
 mkdir -p /opt/rchain/scripts
 curl -o /opt/rchain/scripts/chk-rnode-memory.sh https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/scripts/chk-rnode-memory.sh
 chmod +x /opt/rchain/scripts/chk-rnode-memory.sh
 
-ln -sf /opt/rchain/scripts/chk-rnode-memory.sh /etc/cron.hourly/chk-rnode-memory.sh
+ln -sf /opt/rchain/scripts/chk-rnode-memory.sh /etc/cron.hourly/chk-rnode-memory
