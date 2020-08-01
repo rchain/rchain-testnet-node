d7ddd63ef722e3fc3f8acfd69d7d8d263c6e61a1
GSJ <17771847+gsj5@users.noreply.github.com>
2020-08-01 02:46:18 -0400

    add s3fs and chk-rnode-memory.sh

--- node-files.mainnet-obs/bootstrap.mainnet-observer.sh
+++ node-files.mainnet-obs/bootstrap.mainnet-observer.sh
@@ -4,15 +4,22 @@ set -e
 
 apt update
 apt -y upgrade 
-apt -y install docker.io docker-compose collectd certbot s3fs
+apt -y install docker.io docker-compose collectd certbot bc openjdk-11-jdk-headless
 
 # Mount IBM Cloud bucket
-mkdir -p /mnt/rchain-bucket
-s3fs rchain-heapdumps /mnt/rchain-bucket -o url="https://s3.private.eu.cloud-object-storage.appdomain.cloud" -o passwd_file=/root/rchain-bucket.key -o dbglevel=info
+curl -o /tmp/s3fs_1.86+git-v1.86-2_amd64.deb https://media.githubusercontent.com/media/maresb/docker-build-s3fs/master/builds/s3fs_1.86+git-v1.86-2_amd64.deb
+dpkg -i /tmp/s3fs_1.86+git-v1.86-2_amd64.deb
+mkdir -p /mnt/rchain-heapdumps
+s3fs rchain-heapdumps /mnt/rchain-heapdumps -o url="https://s3.private.eu.cloud-object-storage.appdomain.cloud",passwd_file=/root/rchain-bucket.key,use_cache=/rchain,parallel_count=5,multipart_size=1000
+
+mkdir -p /opt/rchain/scripts
+curl -o /opt/rchain/scripts/chk-rnode-memory.sh https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/scripts/chk-rnode-memory.sh
+ln -sf  /opt/rchain/scripts/chk-rnode-memory.sh /etc/cron.hourly/chk-rnode-memory
 
 curl -o /etc/collectd/collectd.conf  https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/collectd.conf
 curl -o /var/lib/rnode-static/logback.xml https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/logback.xml
 curl -o /var/lib/rnode/docker-compose.yml https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/node-files.mainnet-obs/docker-compose.yml
+sed -i "s/\${HOST_NAME}/$(hostname -f)/" /var/lib/rnode/docker-compose.yml
 
 (cd /var/lib/rnode; docker-compose up -d rnode)
 curl https://raw.githubusercontent.com/rchain/rchain-testnet-node/dev/rundeck-scripts/provision_nginx_proxy.sh | bash
