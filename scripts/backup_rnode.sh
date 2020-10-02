5f1b89537dd1d1f48407db586c8fc4bbd85fe03f
GSJ <17771847+gsj5@users.noreply.github.com>
2020-10-01 22:45:15 -0400

    support multi rnode setup

--- scripts/backup_rnode.sh
+++ scripts/backup_rnode.sh
@@ -1,55 +1,78 @@
 #!/bin/bash
 
-set -e
-trap 'catch_errors $? $LINENO' EXIT
+#set -e
+
+if [ $# -ne 2 ]; then
+	echo "usage: $(basename $0) <rnode-container-name> <rnode-dir>"
+	echo "Example: $(basename $0) mainnet-observer-dev /rchain/mainnet/observer-dev"
+	exit
+fi
 
 ALERT_URL="https://discord.com/api/webhooks/745631958700261386/lQMlhZw5Q80JrnNLbfpwi-AtDacTEf3-81mcv9rOBOvyRp0TiCmW3uMhHyaPGhq8PUjI"
 PATH="$PATH:/usr/local/bin"
 DT=$(date -u +%g%m%d-%H%M)
 
-TARDIR="/rchain"
-TARFILE_PREFIX="rnode-backup"
-TARFILE="$TARFILE_PREFIX-$DT.tar.gz"
-LOCKFILE="$TARDIR/$TARFILE_PREFIX.LOCK"
-BACKUP_DIR="/mnt/heapdumps/$(hostname -f)"
+RNODE_NETWORK="$1"
+RNODE_NAME="$2"
+
+RNODE_CONTAINER_NAME="${RNODE_NETWORK}-${RNODE_NAME}"
+RNODE_DIR="/rchain/${RNODE_NETWORK}/${RNODE_NAME}"
+TARFILE="$RNODE_CONTAINER_NAME-$DT.tar.gz"
+LOCKFILE="$RNODE_DIR/backing-up-$RNODE_NAME.LOCK"
+BACKUP_DIR="/mnt/heapdumps/$RNODE_NETWORK"
+S3FS_DIR="/rchain/rchain-heapdumps"
 
 # Catch script errors and send discord alert
+trap 'catch_errors $? $LINENO' EXIT
 catch_errors() {
-	if [ "$1" != "0" ]; then
-		msg="{\"content\": \"$(hostname -f):$0: Backup failed with error <$1> on line $2.\"}"
-		/usr/bin/curl --silent -H "Content-Type: application/json" -d "$msg" "$ALERT_URL"
-	fi
+        if [ "$1" != "0" ]; then
+		msg="{\"content\": \"$(hostname -f):$(basename $0): Backup failed with error <$1> on line $2.\"}"
+                ###/usr/bin/curl --silent -H "Content-Type: application/json" -d "$msg" "$ALERT_URL"
+                echo "/usr/bin/curl --silent -H 'Content-Type: application/json' -d $msg $ALERT_URL"
+        fi
 }
 
 # Redirect all outout to syslog and stderr
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
-echo "Starting rnode backup"
+# Do some error checking
+if [[ -z "$(docker ps -q -f name=${RNODE_CONTAINER_NAME})" ]]; then
+	echo "Error: Docker container $RNODE_CONTAINER_NAME doesn't exist.  Exiting..."
+	exit
+fi
+
+if [[ ! -d $RNODE_DIR ]]; then
+	echo "Error: Directory $RNODE_DIR does not exist.  Exiting..."
+	exit
+fi
+
+echo "Starting $RNODE_NETWORK-$RNODE_NAME backup"
 
 # Check if this script is already running?
 if [[ -f "$LOCKFILE" ]]; then
         catch_errors "Previous run $LOCKFILE still exists!  Please remove the file after investigation.  Exiting..."  "$LINENO"
+	exit
 else
         echo "Creating $LOCKFILE"
         touch $LOCKFILE
 fi
 
-echo "Stopping rnode"
-docker stop rnode && docker rm rnode
+echo "Stopping $RNODE_CONTAINER_NAME"
+docker stop $RNODE_CONTAINER_NAME && docker rm $RNODE_CONTAINER_NAME
 
-echo "Archiving rnode database into $TARFILE"
-(cd $TARDIR;tar -I "pigz --best --recursive" -cvf $TARFILE rnode/{blockstore,dagstorage,rspace,last-finalized-block,casperbuffer})
+echo "Archiving $RNODE_NAME database into $TARFILE"
+(cd $RNODE_DIR;tar -I "pigz --best --recursive" -cvf $TARFILE rnode/{blockstore,dagstorage,rspace,last-finalized-block,casperbuffer})
 
-echo "Starting rnode"
-(cd /var/lib/rnode/;docker-compose up -d rnode)
+echo "Starting $RNODE_CONTAINER_NAME"
+(cd $RNODE_DIR/..;docker-compose up -d $RNODE_CONTAINER_NAME)
 
-FILESIZE=$(stat -c "%s" $TARDIR/$TARFILE)
+FILESIZE=$(stat -c "%s" $RNODE_DIR/$TARFILE)
 echo "Moving $TARFILE (Size=$FILESIZE) to heapdumps bucket"
 mkdir -p $BACKUP_DIR
-cp $TARDIR/$TARFILE $BACKUP_DIR && rm $TARDIR/$TARFILE
+cp $RNODE_DIR/$TARFILE $BACKUP_DIR && rm $RNODE_DIR/$TARFILE && rm $S3FS_DIR/$RNODE_NETWORK/$TARFILE
 
-echo "Cleaning up backup files older then 7 days"
-find $BACKUP_DIR -name $TARFILE_PREFIX*.tar.gz -mtime +7 -exec rm {} \;
+echo "Cleaning up backup files older then 3 days"
+find $BACKUP_DIR -name $RNODE_CONTAINER_NAME*.tar.gz -mtime +3 -print -exec rm {} \;
 
 rm $LOCKFILE
 echo "All Done!"
